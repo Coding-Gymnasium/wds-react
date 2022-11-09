fe30d0e80ac2fa6c44fb0e2394b33ac469406e2f
Nico Rithner <nicorithner@gmail.com>
2022-11-08 17:47:38 -0700

    Set up react router and routes
    Add active path highlight using hooks

--- react-navbar/src/App.js
+++ react-navbar/src/App.js
@@ -2,26 +2,18 @@ import Navbar from "./Navbar";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Pricing from "./pages/Pricing";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
-  let Component;
-  switch (window.location.pathname) {
-    case "/":
-      Component = Home;
-      break;
-    case "/pricing":
-      Component = Pricing;
-      break;
-    case "/about":
-      Component = About;
-      break;
-  }
-
   return (
     <>
       <Navbar />
       <div className="container">
-        <Component />
+        <Routes>
+          <Route path="/" element={<Home/>} />
+          <Route path="/pricing" element={<Pricing/>} />
+          <Route path="/about" element={<About/>} />
+        </Routes>
       </div>
     </>
   );
